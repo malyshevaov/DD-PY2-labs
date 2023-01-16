bacc805947ab966ebf1e37a4604d763eac68d868
malyshevaov <115467756+malyshevaov@users.noreply.github.com>
2023-01-17 01:22:56 +0300

    Add files via upload

---
+++ Лабораторная 2/ЛР2_new/library.py
@@ -0,0 +1,49 @@
+from book import *
+from typing import Optional
+
+BOOKS_DATABASE = [
+    {
+        "id": 1,
+        "name": "test_name_1",
+        "pages": 200,
+    },
+    {
+        "id": 2,
+        "name": "test_name_2",
+        "pages": 400,
+    }
+]
+
+class Library():
+    books: Optional[list]
+
+    def __init__(self, books: list = None):
+        if books is None:
+            books = []
+        self.books = books
+
+    def get_next_book_id(self) -> int:
+        if self.books == []:
+            return 1
+        else:
+            id_number = 0
+            for i,value in enumerate(self.books):
+                id_number += 1
+            return id_number + 1
+
+    def get_index_by_book_id (self, id_) -> int:
+        if id_ - 1 <= len([self.books]):
+            return id_ - 1
+        else:
+            raise ValueError("Книги с запрашиваемым id не существует")
+
+if __name__ == '__main__':
+    empty_library = Library()  # инициализируем пустую библиотеку
+    print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
+
+    list_books = [
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+    ]
+    library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
+    print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
+    print(library_with_books.get_index_by_book_id(1))  # проверяем индекс книги с id = 1
